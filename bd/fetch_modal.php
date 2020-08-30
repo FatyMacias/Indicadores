4fb035b051057af2b7d6d45ff231874be2880862
JorgeLTC <30934020+JorgeLTC@users.noreply.github.com>
2020-08-29 21:12:51 -0500

    se creo un nuevo fetch

---
+++ bd/fetch_modal.php
@@ -0,0 +1,32 @@
+<?php
+
+//fetch.php
+
+include('database_connection.php');
+
+if(isset($_POST["id"]))
+{
+ $query = "
+ SELECT mes,cve_cpto,cat_conceptos.concepto AS nomcom,per_ded,qna_pago,SUM(importe) AS 'total' FROM indicador JOIN cat_mes ON indicador.qna_pago = cat_mes.id_quin JOIN cat_conceptos ON indicador.concepto = cat_conceptos.cve_cpto 
+ WHERE SUBSTRING(qna_pago,1,4) = '".$_POST["id"]."'
+ GROUP BY mes ORDER BY qna_pago ASC
+ ";
+ $statement = $connect->prepare($query);
+ $statement->execute();
+ $result = $statement->fetchAll();
+
+ 
+ foreach($result as $row)
+ {
+  $output[] = array(
+   'concepto'   => $row["mes"],
+    'clave'   => $row["cve_cpto"],
+    'nombre'  => $row["nomcom"],
+    'deduc'   => $row["per_ded"],
+   'importe'  => floatval($row["total"])
+  );
+ }
+ echo json_encode($output);
+}
+
+?>
