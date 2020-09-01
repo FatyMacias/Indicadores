ac2aff36475b9f05a6f54a2a1ac98006edf8f3dd
JorgeLTC <30934020+JorgeLTC@users.noreply.github.com>
2020-08-31 23:43:42 -0500

    este no es bueno

--- bd/fetch_modal.php
+++ bd/fetch_modal.php
@@ -10,9 +10,9 @@ include('database_connection.php');
 if(isset($_POST["id"]))
 {
  $query = "
- SELECT cve_cpto,cat_conceptos.concepto AS nomcom,per_ded,qna_pago,SUM(importe) AS 'total' FROM indicador JOIN cat_conceptos ON indicador.concepto = cat_conceptos.cve_cpto 
+ SELECT mes,cve_cpto,cat_conceptos.concepto AS nomcom,per_ded,qna_pago,SUM(importe) AS 'total' FROM indicador JOIN cat_mes ON indicador.qna_pago = cat_mes.id_quin JOIN cat_conceptos ON indicador.concepto = cat_conceptos.cve_cpto 
  WHERE SUBSTRING(qna_pago,1,4) = '".$_POST["id"]."'
- group BY cve_cpto
+ GROUP BY mes ORDER BY qna_pago ASC
  ";
  $statement = $connect->prepare($query);
  $statement->execute();
